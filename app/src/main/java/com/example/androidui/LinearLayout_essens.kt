f10db82f4dd4518762044e2771b94f414d0460bb
chaidanSnru <chaidan@snru.ac.th>
2025-07-22 17:06:26 +0700

    ทำเพิ่ม linearlayout อีก 2 layout ที่แสดง weight และที่ essential

---
+++ app/src/main/java/com/example/androidui/LinearLayout_essens.kt
@@ -0,0 +1,20 @@
+package com.example.androidui
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class LinearLayout_essens : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_linear_layout_essens)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
