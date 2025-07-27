fb18120fcbddb1aa20b59c43f777f5a847c8f264
chaidanSnru <chaidan@snru.ac.th>
2025-07-27 12:40:07 +0700

    ทำการเพิ่ม FrameLayout อีก 2 Layout ที่แสดง Example และ Essential

---
+++ app/src/main/java/com/example/androidui/frameLayout_Exam.kt
@@ -0,0 +1,20 @@
+package com.example.androidui
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class frameLayout_Exam : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_frame_layout_exam)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
